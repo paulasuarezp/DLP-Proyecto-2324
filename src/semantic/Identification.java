7d37486a2a6fbf4927df18a02756a2234b6d3311
Paula Suárez <paulasp24@icloud.com>
2024-03-12 12:18:28 +0100

    clase 7

--- src/semantic/Identification.java
+++ src/semantic/Identification.java
@@ -46,4 +46,35 @@ public class Identification extends DefaultVisitor {
         notifyError(msg, node.start());
     }
 
+    /**
+     * predicate. Auxiliary method to implement predicates. Delete if not needed.
+     *
+     * Usage examples:
+     *
+     *    predicate(expr.type != null), "Type cannot be null", expr.start());
+     *
+     *    predicate(expr.type != null), "Type cannot be null", expr);       // expr.start() is assumed
+     *
+     * The start() method (example 1) indicates the position in the file where the node was. If VGen is used, this method
+     * will have been generated in all AST nodes.
+     *
+     * @param condition     Must be met to avoid an error
+     * @param errorMessage  Printed if the condition is not met
+     * @param errorPosition Row and column in the file where the error occurred.
+     * @return true if the condition is met
+     */
+
+    private boolean predicate(boolean condition, String errorMessage, Position position) {
+        if (!condition) {
+            notifyError(errorMessage, position);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean predicate(boolean condition, String errorMessage, AST node) {
+        return predicate(condition, errorMessage, node.start());
+    }
+
 }
