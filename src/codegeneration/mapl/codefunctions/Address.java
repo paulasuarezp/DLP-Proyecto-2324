c1cde32be9399638391ff3b986e0581ceff84378
Paula Suárez <paulasp24@icloud.com>
2024-04-29 18:33:15 +0200

    address

--- src/codegeneration/mapl/codefunctions/Address.java
+++ src/codegeneration/mapl/codefunctions/Address.java
@@ -2,8 +2,10 @@
 
 package codegeneration.mapl.codefunctions;
 
+import ast.Scope;
 import ast.expression.*;
 import codegeneration.mapl.*;
+import codegeneration.mapl.utils.MaplUtils;
 
 
 public class Address extends AbstractCodeFunction {
@@ -12,145 +14,19 @@ public class Address extends AbstractCodeFunction {
         super(specification);
     }
 
-
-	// class IntConstant(String value)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(IntConstant intConstant, Object param) {
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class RealConstant(String value)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(RealConstant realConstant, Object param) {
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class CharConstant(String value)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(CharConstant charConstant, Object param) {
-
-		out("<instruction>");
-
-		return null;
-	}
-
 	// class Variable(String name)
 	// phase Identification { VarDefinition definition }
 	// phase TypeChecking { boolean lvalue, Type type }
 	@Override
 	public Object visit(Variable variable, Object param) {
 
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class CastExpr(Type castType, Expression value)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(CastExpr castExpr, Object param) {
-
-		// value(castExpr.getValue());
-		// address(castExpr.getValue());
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class ArithmeticExpr(Expression op1, String operator, Expression op2)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(ArithmeticExpr arithmeticExpr, Object param) {
-
-		// value(arithmeticExpr.getOp1());
-		// address(arithmeticExpr.getOp1());
-
-		// value(arithmeticExpr.getOp2());
-		// address(arithmeticExpr.getOp2());
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class LogicalExpr(Expression op1, String operator, Expression op2)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(LogicalExpr logicalExpr, Object param) {
-
-		// value(logicalExpr.getOp1());
-		// address(logicalExpr.getOp1());
-
-		// value(logicalExpr.getOp2());
-		// address(logicalExpr.getOp2());
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class ComparationExpr(Expression op1, String operator, Expression op2)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(ComparationExpr comparationExpr, Object param) {
-
-		// value(comparationExpr.getOp1());
-		// address(comparationExpr.getOp1());
-
-		// value(comparationExpr.getOp2());
-		// address(comparationExpr.getOp2());
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class MinusExpr(Expression op)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(MinusExpr minusExpr, Object param) {
-
-		// value(minusExpr.getOp());
-		// address(minusExpr.getOp());
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class NotExpr(Expression op)
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(NotExpr notExpr, Object param) {
-
-		// value(notExpr.getOp());
-		// address(notExpr.getOp());
-
-		out("<instruction>");
-
-		return null;
-	}
-
-	// class FunctionCallExpr(String name, List<Expression> args)
-	// phase Identification { FunctionDefinition definition }
-	// phase TypeChecking { boolean lvalue, Type type }
-	@Override
-	public Object visit(FunctionCallExpr functionCallExpr, Object param) {
-
-		// value(functionCallExpr.args());
-		// address(functionCallExpr.args());
-
-		out("<instruction>");
+		if(variable.getDefinition().getScope() == Scope.GLOBAL)
+			out("PUSHA " + variable.getDefinition().getAddress());
+		else{
+			out("PUSHBP");
+			out("PUSHA " + variable.getDefinition().getAddress());
+			out("ADDI");
+		}
 
 		return null;
 	}
@@ -163,7 +39,9 @@ public class Address extends AbstractCodeFunction {
 		// value(fieldAccess.getRoot());
 		// address(fieldAccess.getRoot());
 
-		out("<instruction>");
+		address(fieldAccess.getRoot());
+		out("PUSHI " + fieldAccess.getRoot().getType());
+		out("ADDI");
 
 		return null;
 	}
@@ -179,7 +57,11 @@ public class Address extends AbstractCodeFunction {
 		// value(arrayAccess.getIndex());
 		// address(arrayAccess.getIndex());
 
-		out("<instruction>");
+		address(arrayAccess.getArray());
+		value(arrayAccess.getIndex());
+		out("PUSHI " + MaplUtils.maplTypeSize(arrayAccess.getType()));
+		out("MULI");
+		out("ADDI");
 
 		return null;
 	}
